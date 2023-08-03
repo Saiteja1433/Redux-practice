ab35415ab1162845bac9884fb78b41fd64231bc2
Saiteja <Saitejacp210@gmail.com>
2023-08-03 12:35:15 +0530

    Added Icecreams to the store, update the reducer function to handle the new state Icecreams, and divided the action objects of the intial states and also divied the reducer fuction

--- index.js
+++ index.js
@@ -19,56 +19,123 @@ const createStore = redux.legacy_createStore;
 
 
 
+//Redux provides the method called the combineReducers() which is used to combine the multiple reducers into one reducer function
+const combineReducers = redux.combineReducers
+
 const BUY_CAKE = "BUY_CAKE"
 
 // this (buyCake) function is the Action Creator
 // Action Creator is a function with returns the Action object
 function buyCake() {
     // this is the REDUX ACTION object with "type" as the property
-    console.log(1)
+    
     return {
         type: BUY_CAKE,
         info: "This is the first Redux Action"
     }
 }
 
-
-
-//this is the initial state of the application which is represented using the javaScript object
-const initialState = {
-    numberOfCakes: 10
+function buyIceCream() {
+    // this is the REDUX ACTION object with "type" as the property
+    
+    return {
+        type: "BUY_ICECREAM",
+        info: "This is the first Redux Action"
+    }
 }
 
-
-
-// function reducer(state= initialState, action){
-//     switch(action.type){
-//         case BUY_CAKE: {
-// ...reducer,
-//             numberOfCakes :  numberOfCakes-1
-//         } 
-//     }
+//this is the initial state of the application which is represented using the javaScript object
+// const initialState = {
+//     numberOfCakes: 10,
+//     numberOfIceCream : 10
 // }
 
 
-// this is the Reducer fucntion, it accepts the 2 parameters by default they are initial state (or state before performing the action)
-// and "Action" is the another parameter
-const reducer = (state = initialState, action) => {
-    console.log(2, action)
+// splited the the above initial state object into two initial state objects
+//by this we can handle each state seperatly with out confusion
+
+
+const initialCakeState ={
+    numberOfCakes: 10
+}
+
+const initialIceCreamState ={
+    numberOfIceCream: 10
+}
+
+
+// // function reducer(state= initialState, action){
+// //     switch(action.type){
+// //         case BUY_CAKE: {
+// // ...reducer,
+// //             numberOfCakes :  numberOfCakes-1
+// //         } 
+// //     }
+// // }
+
+
+// // this is the Reducer fucntion, it accepts the 2 parameters by default they are initial state (or state before performing the action)
+// // and "Action" is the another parameter
+// const reducer = (state = initialState, action) => {
+//     console.log(2, action)
+//     switch (action.type) {
+//         case BUY_CAKE: 
+//             return {
+//                 ...state,
+//                 numberOfCakes: state.numberOfCakes- 1
+//             }
+        
+//         case "BUY_ICECREAM": 
+//             return {
+//                 ...state,
+//                 numberOfIceCream: state.numberOfIceCream- 1
+//             }
+//         break;
+//         default: return state
+//     }
+// }
+
+//converted the above large reducer into the 2 small and individual reducers which can help
+// us in the clear code and look more modular and easy update track the changes easily
+const cakeReducer = (state = initialCakeState, action) => {
     switch (action.type) {
-        case BUY_CAKE: return {
-            ...state,
-            numberOfCakes: state.numberOfCakes- 1
-        }
+        case BUY_CAKE: 
+            return {
+                ...state,
+                numberOfCakes: state.numberOfCakes- 1
+            }
         default: return state
     }
 }
+
+
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+    switch (action.type) {
+        case "BUY_ICECREAM": 
+            return {
+                ...state,
+                numberOfIceCream: state.numberOfIceCream- 1
+            }
+        default: return state
+    }
+}
+
+
+// combineReducer function accepts the object as its parameter, 
+//where this object contains the key:value pair of the all the reducers
+const rootReducer = combineReducers({
+    cake: cakeReducer,
+    iceCream : iceCreamReducer
+})
+// now we pass this rootReducer as the reducer function of the REDUXSTORE
+
+
 
 //createStore (redux.legacy_createStore) is the method, it accpets the reducer function as its parameter
 //reducer function has the initial state of the store and it can perform the different transicitons on the store
 // based up no the action it receives
 // In the below line of code we initilize our redux store holding the application state
-const store = createStore(reducer)
+const store = createStore(rootReducer)
 
 
 
@@ -79,11 +146,17 @@ console.log("state :", store.getState());
 
 //store.subscribe(), subscribe() method is used to subscribe to the store for the changes on the store
 //subscribe is the listener which get called automatically when we update the state of the store
-store.subscribe(() => { console.log("updated state :" ,store.getState())})
+// store.subscribe(() => { console.log("updated state :" ,store.getState())})
+store.subscribe(() =>  console.log("updated state :" ,store.getState()))
 
 
 //store provides a method called the dispatch()  to update the state of the store
 //we have to pass the action object to the dispatch method
 // store.dispatch({type:"BUY_CAKE"}) <-- we can also use this
 store.dispatch(buyCake())
+
+store.dispatch(buyIceCream())
+store.dispatch(buyIceCream())
+store.dispatch(buyIceCream())
+
 
